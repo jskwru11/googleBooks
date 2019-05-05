2980b90cfe689a160b0e331b5c0e3099bc1626dd
John Alexander <jalexander@alexandersworld.biz>
2019-05-05 11:36:28 -0400

    fixing bug for delete book

--- src/app/saved/saved.component.ts
+++ src/app/saved/saved.component.ts
@@ -17,7 +17,7 @@ export class SavedComponent implements OnInit {
   }
 
   onRemove(index: number) {
-    const delBook = this.books[index].id;
+    const delBook = this.books[index].idTag;
     this.http.delBook(delBook)
     .subscribe(data => console.log(data))
   }
